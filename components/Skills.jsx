49762c265f2641cce9e9f3df12afe3dab53452ea
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-30 10:24:01 +0800

    Add skill components

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './config/firebase'
+import { doc } from 'firebase/firestore'
+import { useDocument } from 'react-firebase-hooks/firestore'
+import Image from 'next/image'
 
 const Skills = () => {
-  return <div>Skills</div>
+  const [value, loading, error] = useDocument(
+    doc(db, 'porto', 'iqbalFirdaus'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true }
+    }
+  )
+  return (
+    <div id='skills' className='w-full lg:h-screen p-2 font-raleway'>
+      <div className=' max-w-7xl mx-auto flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest uppercase text-secondary'>
+          Skills
+        </p>
+        <h2 className='py-4'>What I can do</h2>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+          {loading
+            ? null
+            : value.data().data[1].skill.map((skill) => {
+                return (
+                  <>
+                    <div className='p-6 rounded-xl space-y-4 flex flex-col items-center hover:scale-105 ease-in duration-100'>
+                      <div className='m-auto'>
+                        <Image
+                          src={`/..${skill.img}`}
+                          width={64}
+                          height={64}
+                          alt='/'
+                        />
+                      </div>
+                      <div>
+                        <h3>{skill.name}</h3>
+                      </div>
+                    </div>
+                  </>
+                )
+              })}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Skills
--- components/config/firebase.jsx
+++ components/config/firebase.jsx
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_apiKey,
-  authDomain: process.env.FIREBASE_authDomain,
-  projectId: process.env.FIREBASE_projectId,
-  storageBucket: process.env.FIREBASE_storageBucket,
-  messagingSenderId: process.env.FIREBASE_messagingSenderId,
-  appId: process.env.FIREBASE_appId
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId
 }
-
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
