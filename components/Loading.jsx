8815eb23b7308700785f876297e174db429ead57
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-30 23:50:07 +0800

    Change Loading screen

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,15 +1,13 @@
-import { ThreeDots } from 'react-loader-spinner'
+import { RotatingLines } from 'react-loader-spinner'
 
 const Loading = () => {
   return (
     <div className='flex flex-col space-y-9 justify-center text-accent items-center h-screen'>
-      <ThreeDots
-        width={120}
-        radius='10'
-        color='#2b9ea0'
-        ariaLabel='three-dots-loading'
-        wrapperStyle={{}}
-        wrapperClassName=''
+      <RotatingLines
+        strokeColor='grey'
+        strokeWidth='5'
+        animationDuration='0.75'
+        width='96'
         visible={true}
       />
       <h2>Please Wait</h2>
