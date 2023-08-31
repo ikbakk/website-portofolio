2b74f2547e8924d44b45cd82afd80e2d7eaaab27
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-08-31 22:54:01 +0800

    create sanity schemas

---
+++ src/schemas/index.ts
@@ -0,0 +1,6 @@
+import info from './info';
+import socials from './socials';
+import skills from './skills';
+import projects from './projects';
+
+export const schemaTypes = [info, socials, skills, projects];
