f6544b3590ed253359b4025c53661f4315515868
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-09-05 08:01:57 +0800

    remove unused files, code and package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
-import { defineConfig } from 'astro/config';
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import mdx from "@astrojs/mdx";
-const sanityConfig = {
-  projectId: 'ife8w0nb',
-  dataset: 'production',
-  useCdn: false,
-  studioBasePath: '/studio'
-};
+import { defineConfig } from "astro/config";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
+const sanityConfig = {
+  projectId: "ife8w0nb",
+  dataset: "production",
+  useCdn: false,
+  studioBasePath: "/studio",
+};
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ['cdn.sanity.io']
+    domains: ["cdn.sanity.io"],
   },
-  output: 'hybrid',
-  integrations: [sanity(sanityConfig), react(), tailwind(), mdx()]
+  output: "hybrid",
+  integrations: [sanity(sanityConfig), react(), tailwind()],
 });
