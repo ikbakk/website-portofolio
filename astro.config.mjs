6a9bd02fde1809ffd0dcc102df8ef422d89fbe5b
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-09-05 08:08:19 +0800

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 const sanityConfig = {
   projectId: "ife8w0nb",
   dataset: "production",
   useCdn: false,
-  studioBasePath: "/studio",
+  studioBasePath: "/studio"
 };
+
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io"]
   },
   output: "hybrid",
   integrations: [sanity(sanityConfig), react(), tailwind()],
+  adapter: vercel()
 });
