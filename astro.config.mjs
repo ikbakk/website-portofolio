ff56a382e873fff663b0357d3694bb5aabcd0289
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-09-05 08:23:58 +0800

    remove unused package

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
   image: {
     domains: ["cdn.sanity.io"]
   },
-  output: "hybrid",
+  output: "server",
   integrations: [sanity(sanityConfig), react(), tailwind()],
   adapter: vercel()
 });
