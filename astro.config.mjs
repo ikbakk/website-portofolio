3b187adc089d52a55ea82f987fc614a02deb3382
Muhammad Iqbal Firdaus <iqbalfirdaus05@gmail.com>
2023-09-06 16:52:50 +0800

    add prefetch option

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import prefetch from "@astrojs/prefetch";
 const sanityConfig = {
   projectId: "ife8w0nb",
   dataset: "production",
@@ -16,7 +17,7 @@ export default defineConfig({
   image: {
     domains: ["cdn.sanity.io"]
   },
-  output: "server",
-  integrations: [sanity(sanityConfig), react(), tailwind()],
+  output: "hybrid",
+  integrations: [sanity(sanityConfig), react(), tailwind(), prefetch()],
   adapter: vercel()
 });
